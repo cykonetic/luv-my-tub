1ff58059ea83618c9b5ff58e303eeeab91f7c30b
Nicholai Bush <nicholaibush@yahoo.com>
2019-05-26 15:52:56 -0400

    This way worked first

--- src/app.js
+++ src/app.js
@@ -1,16 +1,9 @@
 'use strict'
 
-require('dotenv').config()
-console.log('document root is ' + process.env.DOCUMENT_ROOT)
-
 const express = require('express')
 var app = express()
 
-app.get('/', function(req, res) {
-    console.log('document root is ' + process.env.DOCUMENT_ROOT)
-    
-    res.sendFile('/../html/index.html', {root: process.env.DOCUMENT_ROOT})
-})
+app.use(express.static('public'))
 
 app.listen(3000, function() {
   console.log('listening on port 3000')
